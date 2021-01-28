d83205b323a4dd4b90f99185516c611b05d08f3c
abdullah <abdullah3553>
2021-01-28 02:55:29 +0200

    Almost fully function start window

--- bbl_sort.py
+++ bbl_sort.py
@@ -3,80 +3,77 @@ from tkinter import messagebox
 import random
 
 
-main_window = Tk()
-lop1 = 0
-lop2 = lop1 + 1
-is_sorted = False
-cur_op = 1
+def main(arr_size, rand_choice):
+    main_window = Tk()
+    lop1 = 0
+    lop2 = lop1 + 1
+    is_sorted = False
+    cur_op = 1
 
+    def comp_check(element1, element2):
+        if int(element1['text']) < int(element2['text']):
+            op_lbl["text"] = f"{element1['text']} swapped with {element2['text']}"
+            return element2['text'], element1['text'], element2['width'], element1['width']
+        op_lbl["text"] = "No swap happened"
+        return element1['text'], element2['text'], element1['width'], element2['width']
 
-def comp_check(element1, element2):
-    if int(element1['text']) < int(element2['text']):
-        op_lbl["text"] = f"{element1['text']} swapped with {element2['text']}"
-        return element2['text'], element1['text'], element2['width'], element1['width']
-    op_lbl["text"] = "No swap happened"
-    return element1['text'], element2['text'], element1['width'], element2['width']
-
-
-def one_op(txt):
-    global lop1, lop2, is_sorted, cur_op
-    if is_sorted:
-        for k in range(arr_size-1):
-            if int(arr_lbl[k]['text']) <= int(arr_lbl[k+1]['text']):
-                is_sorted = True
-            else:
-                is_sorted = False
-                break
+    def one_op():
+        nonlocal lop1, lop2, is_sorted, cur_op
         if is_sorted:
-            messagebox.showinfo("Yay :d", "The array is sorted !")
-            return
+            for k in range(arr_size-1):
+                if int(arr_lbl[k]['text']) <= int(arr_lbl[k+1]['text']):
+                    is_sorted = True
+                else:
+                    is_sorted = False
+                    break
+            if is_sorted:
+                messagebox.showinfo("Yay :d", "The array is sorted !")
+                return
 
-    if cur_op == 1:
-        if lop1 < arr_size and lop2 < arr_size:
-            compr_statmnt_lbl["text"] = f"{arr_lbl[lop1]['text']} is compared with {arr_lbl[lop2]['text']}"
-            op_lbl['text'] = "Comparing operation"
-        else:
-            op_lbl['text'] = "Compare Check operation"
-            if lop1 >= arr_size-1 and lop2 >= arr_size-1:
-                is_sorted = True
-        cur_op = 0
-
-    else:
-        if lop1 < arr_size:
-            if lop2 < arr_size:
-                arr_lbl[lop2]['text'], arr_lbl[lop1]['text'], arr_lbl[lop2]['width'], arr_lbl[lop1]['width'] =\
-                    comp_check(arr_lbl[lop2], arr_lbl[lop1])
-                lop2 += 1
+        if cur_op == 1:
+            if lop1 < arr_size and lop2 < arr_size:
+                compr_statmnt_lbl["text"] = f"{arr_lbl[lop1]['text']} is compared with {arr_lbl[lop2]['text']}"
+                op_lbl['text'] = "Comparing operation"
             else:
-                op_lbl["text"] = "Loop Check operation"
-                lop1 += 1
-                lop2 = lop1 + 1
+                op_lbl['text'] = "Compare Check operation"
+                if lop1 >= arr_size-1 and lop2 >= arr_size-1:
+                    is_sorted = True
+            cur_op = 0
+
         else:
-            is_sorted = True
-        cur_op = 1
+            if lop1 < arr_size:
+                if lop2 < arr_size:
+                    arr_lbl[lop2]['text'], arr_lbl[lop1]['text'], arr_lbl[lop2]['width'], arr_lbl[lop1]['width'] =\
+                        comp_check(arr_lbl[lop2], arr_lbl[lop1])
+                    lop2 += 1
+                else:
+                    op_lbl["text"] = "Loop Check operation"
+                    lop1 += 1
+                    lop2 = lop1 + 1
+            else:
+                is_sorted = True
+            cur_op = 1
 
+    # array initialization
+    arr = []
+    maxm_num = -1
+    if rand_choice:
+        for i in range(arr_size):
+            arr.append(random.randint(0, 50))
+            if arr[len(arr)-1] > maxm_num:
+                maxm_num = arr[len(arr)-1]
 
-# array initialization
-arr = []
-maxm_num = -1
-arr_size = int(input("Enter the size of the array : "))
-for i in range(arr_size):
-    arr.append(random.randint(0, 50))
-    if arr[len(arr)-1] > maxm_num:
-        maxm_num = arr[len(arr)-1]
-    # arr.append(int(input("> ")))
-
-#  Labels initialization and printing
-arr_lbl = []
-for i in range(arr_size):
-    arr_lbl.append(Label(text=str(arr[i]), width=arr[i], bg='blue'))
-for i in range(arr_size):
-    arr_lbl[i].grid(row=i, column=0, pady=1)
-btn_1 = Button(text="Next", command=lambda: one_op("test"))
-btn_1.grid(row=arr_size+2, column=0, sticky="nwse")
-op_lbl = Label(text='No Operation ...')
-op_lbl.grid(row=arr_size+1, column=0)
-compr_statmnt_lbl = Label(text='1st element is compared with 2nd element')
-compr_statmnt_lbl.grid(row=arr_size, column=0)
-main_window.columnconfigure(0, minsize=maxm_num)
-main_window.mainloop()
+    #  Labels initialization and printing
+    arr_lbl = []
+    for i in range(arr_size):
+        arr_lbl.append(Label(text=str(arr[i]), width=arr[i], bg='blue'))
+    for i in range(arr_size):
+        arr_lbl[i].grid(row=i, column=0, pady=1)
+    btn_1 = Button(text="Next", command=one_op)
+    btn_1.grid(row=arr_size+2, column=0, sticky="nwse")
+    op_lbl = Label(text='No Operation ...')
+    op_lbl.grid(row=arr_size+1, column=0)
+    compr_statmnt_lbl = Label(text='1st element is compared with 2nd element')
+    compr_statmnt_lbl.grid(row=arr_size, column=0)
+    main_window.columnconfigure(0, minsize=maxm_num)
+    main_window.mainloop()
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import messagebox
+import bbl_sort
 
 # Initializations ;
 start_window = Tk()
@@ -9,23 +11,57 @@ choosen_algo_val = StringVar()
 algo_rdio_frm = Frame(master=start_window)
 bbl_sort_rdio = Radiobutton(master=algo_rdio_frm, text="Bubble Sort", variable=choosen_algo_val, value="bbl_sort"
                             , selectcolor="grey")
+# bbl_sort_rdio2 = Radiobutton(master=algo_rdio_frm, text="None", variable=choosen_algo_val, value="none"
+#                              , selectcolor="grey")
+array_sz = 0
+rand_choice = ""
 
 
 def ok_btn_comm():
-    print(choosen_algo_val.get())
+    if choosen_algo_val.get() != "none":
+        size_enter_window = Tk()
+        rand_choice_in = BooleanVar(master=size_enter_window)
+        array_sz_in = IntVar(master=size_enter_window)
+        size_entry_frm = Frame(master=size_enter_window, borderwidth=1)
+        size_entry = Entry(master=size_entry_frm, width=5, textvariable=array_sz_in)
+        size_entry_lbl = Label(master=size_entry_frm, text="Enter the array size")
+
+        def choosen_ok_btn_comm():
+            try:
+                global array_sz, rand_choice
+                rand_choice = bool(rand_choice_in.get())
+                array_sz = int(array_sz_in.get())
+                size_enter_window.destroy()
+                start_window.destroy()
+                bbl_sort.main(array_sz, rand_choice)
+            except ValueError:
+                messagebox.showerror(master=size_enter_window, title="Wrong Entry", message="Wrong size entered !")
+
+        rand_choice_chk = Checkbutton(master=size_enter_window, text="Random Array", onvalue=True, offvalue=False
+                                      , variable=rand_choice_in, selectcolor="grey")
+        choosen_ok_btn = Button(master=size_enter_window, text="OK", command=choosen_ok_btn_comm, width=10)
+        size_entry_frm.grid(row=1, column=1, padx=5, pady=5)
+        size_entry_lbl.grid(row=1, column=0, padx=5)
+        size_entry.grid(row=1, column=1)
+        rand_choice_chk.grid(row=1, column=2)
+        choosen_ok_btn.grid(row=2, column=2)
+        size_enter_window.mainloop()
+    else:
+        messagebox.showerror(title="Wrong entry", message="You did not choose anything !")
 
 
+# setting Variables ;
 hola_lbl_frm.grid(row=0, column=2, padx=10, pady=10)
 hola_lbl.grid(row=0, column=2, sticky="nesw")
 choose_lbl.grid(row=1, column=2)
 choosen_algo_val.set("none")
 algo_rdio_frm.grid(row=2, column=2)
 bbl_sort_rdio.grid(row=2, column=1)
-# bbl_sort_rdio2 = Radiobutton(master=algo_rdio_frm, text="None", variable=choosen_algo_val, value="none"
-#                              , selectcolor="grey")
 # bbl_sort_rdio2.grid(row=2, column=0)
 ok_btn = Button(text='OK', command=ok_btn_comm)
 ok_btn.grid(row=3, column=2)
 
 
 start_window.mainloop()
+
+
